fb1c26f0c582ebbd7823ceacd9f2abb105aa8450
AXEL AYALA CORDOVA <AAYALA@sirius.local>
2022-05-20 14:01:07 +0200

    bug fixing with reserved fibers

--- main.js
+++ main.js
@@ -214,6 +214,99 @@ const main = async () => {
         }
 
 
+        //reserve on root 
+        if (key.name === 't') {
+            console.log(`You have chosen to reserve on root`);
+            let fibers = []
+
+            const {
+                fork
+            } = require('child_process');
+
+            const child_process = fork('./test.js');
+
+            // Send the data to forked process
+            child_process.on('message', async (data) => {
+                fibers = data.fibers;
+
+                const args = {
+                    root: root.name,
+                    fibers: fibers
+                }
+
+                await apex.evaluate((args) => {
+                    const reserveFibers = (id, reserved) => {
+
+                        const fibers = Icx_Connector.objects.filter(obj => obj.pare.name == id)
+
+                        fibers.forEach((fiber, index) => {
+                            let updated = fiber.element;
+                            updated.reserved_b = true;
+
+                            if (index == reserved[index] - 1) {
+                                Icx_Interconexions.prototype.doUpdateFiber(updated, () => {});
+                            }
+
+                        })
+                    }
+
+                    reserveFibers(args.root, args.fibers)
+
+                }, args);
+
+            });
+
+
+            displayElementList(newList, index)
+        }
+
+
+        if (key.name === 'v') {
+            console.log(`You have chosen to reserve on root`);
+            let fibers = []
+
+            const {
+                fork
+            } = require('child_process');
+
+            const child_process = fork('./test.js');
+
+            // Send the data to forked process
+            child_process.on('message', async (data) => {
+                fibers = data.fibers;
+
+                const args = {
+                    root: root.name,
+                    fibers: fibers
+                }
+
+                await apex.evaluate((args) => {
+                    const reserveFibers = (id, reserved) => {
+
+                        const fibers = Icx_Connector.objects.filter(obj => obj.pare.name == id)
+
+                        fibers.forEach((fiber, index) => {
+                            let updated = fiber.element;
+                            updated.reserved_a = true;
+
+                            if (index == reserved[index] - 1) {
+                                Icx_Interconexions.prototype.doUpdateFiber(updated, () => {});
+                            }
+
+                        })
+                    }
+
+                    reserveFibers(args.root, args.fibers)
+
+                }, args);
+
+            });
+
+
+            displayElementList(newList, index)
+        }
+
+
 
 
 
