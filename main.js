d4b7806388215b46ae55f8dbf81264312c95ffba
aayalacdv <aayalacdv2000@gmail.com>
2022-06-02 13:46:55 +0200

    added function to order cables

--- main.js
+++ main.js
@@ -154,6 +154,52 @@ const main = async () => {
                     }
                 })
 
+
+                const positionElements = (root) => {
+
+                    const rootElement = Icx_Connector.objects.filter((obj) => obj.pare instanceof Icx_Cable && obj.pare.name === root);
+                    const splice = 'xxxx-f5c5-be993402-9bd8c513-85ba2423';
+                    const slitter = 'xxxx-8788-f90c2b03-476836c6-b02fce34';
+
+
+                    let verticalOffset = 0
+                    let horizontalOffset = 1000
+
+                    rootElement.forEach((fiber, index) => {
+
+                        if (fiber.conexio && fiber.conexio.tipoConector === splice) {
+                            const conectorId = fiber.conexio.conector2.pare.name
+                            const conectorElement = interconexionsIU.interconexions.objs.filter((obj) => obj.name === conectorId)[0];
+                            const { x, y } = interconexionsIU.interconexions.objs.filter((obj) => obj.name === root)[0].rect;
+                            const { h, w } = conectorElement.rect
+
+                            const xPos = x + horizontalOffset
+                            const yPos = y - verticalOffset
+                            conectorElement.rect.setPos(xPos, yPos, xPos + w, yPos + h)
+                            verticalOffset -= 200
+                            horizontalOffset -= 300
+
+                        }
+
+                        if (fiber.conexio && fiber.conexio.tipoConector == slitter) {
+                            const slitterId = fiber.conexio.conector2.pare.name
+                            const sitterConnector = interconexionsIU.interconexions.objs.filter((obj) => obj.name === slitterId)[0];
+
+                            const { x, y } = interconexionsIU.interconexions.objs.filter((obj) => obj.name === root)[0].rect;
+                            const { h, w } = sitterConnector.rect
+
+                            const xPos = x - 600
+                            const yPos = y
+
+                            sitterConnector.rect.setPos(xPos, yPos, xPos + w, yPos + h)
+
+                        }
+                    })
+
+                }
+
+                positionElements(args.root); 
+
             }, args);
 
 
@@ -315,22 +361,23 @@ const slitterRemainingFibers = () => {
 
 }
 let Style = {
-    padding : 1
+    padding: 1,
+    borderStyle: 'round'
 }
 
 
 const getBoxenColor = (type) => {
     if (type === 'Cable 48 f.o.') {
-        return {...Style, backgroundColor: 'red'} 
+        return { ...Style, backgroundColor: 'red' }
     }
     if (type === 'Cable 12 f.o.') {
-        return {...Style, backgroundColor: '#ffa500'} 
+        return { ...Style, backgroundColor: '#ffa500' }
     }
 
     if (type === 'splitter') {
-        return {...Style, backgroundColor: 'gray'} 
+        return { ...Style, backgroundColor: 'gray' }
     }
-    return {...Style, backgroundColor: 'yellow'} 
+    return { ...Style, backgroundColor: 'yellow' }
 
 }
 
